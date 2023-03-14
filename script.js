bbdc70a9cf4a3f17919097a1e188351a07ecad7d
Mohamed Abdelghafour <67030318+malgafour@users.noreply.github.com>
2023-03-15 00:58:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+    setTimeout(function() {
+        $('#demo').typed({
+          strings: [
+           " هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها."
+          ],
+          typeSpeed: 5,
+          contentType: 'html'
+        });
+      }, 500);
+ 
+    $(document).ready(function(){
+          
+        $("#more").click(function() {
+            $('html, body').animate({
+                scrollTop: $("#products").offset().top
+            }, 2000);
+        });
+    })
